c9991b10bc798b6a4208106ed6129232aa13626b
Octowl <alsaff1987@gmail.com>
2018-11-06 16:19:45 +0300

    Signup action

--- src/Signup.js
+++ src/Signup.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+// Actions
+import * as actionCreators from "./store/actions";
 
 class Signup extends Component {
   state = {
@@ -12,11 +16,12 @@ class Signup extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    console.log(this.state);
+    this.props.signup(this.state, this.props.history);
   };
 
   render() {
     const { username, email, password } = this.state;
+
     return (
       <div className="col-6 mx-auto">
         <div className="card my-5">
@@ -70,4 +75,12 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+const mapDispatchToProps = dispatch => ({
+  signup: (userData, history) =>
+    dispatch(actionCreators.signup(userData, history))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Signup);
