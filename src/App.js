a905235c779b2f6045fd8faee7cb7b7147bf9cb7
Octowl <alsaff1987@gmail.com>
2018-11-06 16:18:15 +0300

    Private routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 // Components
+import PrivateRoute from "./PrivateRoute";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import Signup from "./Signup";
@@ -17,7 +18,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/garbage" component={Garbage} />
-            <Route path="/treasure" component={Treasure} />
+            <PrivateRoute path="/treasure" component={Treasure} />
             <Route path="/signup" component={Signup} />
             <Redirect to="/" />
           </Switch>
